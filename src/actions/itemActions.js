ad2184d5e34ad5a15c4581e75aaf64efb1e3cdfd
matukai <matt.tanji@gmail.com>
2018-03-05 12:29:54 -1000

    front end form successfully posting to db... some bugs

--- src/actions/itemActions.js
+++ src/actions/itemActions.js
@@ -4,19 +4,40 @@ export const CREATE_ITEM = 'CREATE_ITEM';
 export const EDIT_ITEM = 'EDIT_ITEM';
 
 export const createItem = (newItem) => {
-  console.log('CREATE ITEM BEFORE POST', newItem)
-  axios.post('http://localhost:8080/api/items/new', {
+  console.log('CREATE ITEM BEFORE POST', newItem.price)
+  console.log
+  
+  const test = {
+    name : newItem.name,
+    price : newItem.price,
+    condition_id : 2,
+    category_id : 1,
+    item_status_id : 2,
+    model : "ikea",
+    user_id : 1,
+    dimensions : "420 x 420",
+    notes : "you sit in it",
+    image : "/image"
+  }
+
+
+  axios.post('http://localhost:8080/api/items/new',
+  {
     name: newItem.name,
     image: newItem.image,
     price: newItem.price,
-    condition: newItem.condition,
-    category: newItem.category,
+    condition_id: 1,
+    category_id: 1,
+    item_status_id: 1,
     model: newItem.model,
+    user_id: 1,
     dimensions: newItem.dimensions ,
     notes: newItem.notes
-  })
+  }
+)
   .then((response) => {
     console.log(response)
+    return response.json()
   })
   .catch((err) => {
     console.log(err)
