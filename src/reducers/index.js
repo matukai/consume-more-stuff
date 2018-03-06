961bed044c1c06ff37cc9580765dd1e3d7f2a092
Levi B. Porter <levi@levibrooke.com>
2018-03-05 16:05:30 -1000

    Register user (#12)
    * Created userReducer and added it to index to be combined
    
    * Created user_actions file
    
    * Added axios to packages
    
    * Added Route for /register to header'
    
    * Added action for add user
    
    * Set return for ADD_USER
    
    * Added middleware to allow cross-origin requests
    
    * Created RegisterUser view, changed data sent w/ axios request in action
    
    * Deleted duplicate code
    
    * Added redirectCallback to handle redirect after registration
    
    * Combined input handlers into one method, added redirectCallback to registerUser

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers} from 'redux'
 import itemReducer from './itemReducer';
+import userReducer from './userReducer';
 export default combineReducers({
-  items: itemReducer
+  items: itemReducer,
+  users: userReducer
 })
