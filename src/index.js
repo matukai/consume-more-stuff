5435e82e2c7b794bb252b24a5fdcfff20f55e6d2
Levi B. Porter <levi@levibrooke.com>
2018-03-07 14:08:30 -1000

    Base styles (#21)
    * Revised sass-chokidar so that builder and watcher look for app.scss inside /src/scss
    
    * Deleted index.css and removed import from index.js, moved app.scss to src/scss/
    
    * Cleaned up unused imports
    
    * Moved App.scss into /components, imported App.scss into styles.scss
    
    * Changed App/index.js to pull in styles.css, added reset to partials

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 
 import rootReducer from './reducers';
-import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
